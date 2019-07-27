d7f87662cd6b96565d06891c294b8342293bf864
MechanicalPython <matt.barson@gmail.com>
2019-07-27 21:03:41 +0100

    Init commit

---
+++ humidor/response_bot.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python3
+
+"""
+Montor the slack channel and reply with the current humidity and temp to 'report' command
+"""
+
+import os
+import time
+import pickle
+from subprocess import call
+from slackclient import SlackClient
+from humidor import get_slack_client_id, send_message
+
+RTM_READ_DELAY = 1  # 1 sec delay between reading from RTM
+dir_path = os.path.dirname(os.path.realpath(__file__))
+data_file = "{}/data.pkl".format(dir_path)
+
+
+def get_latest_reading():
+    data = None
+    while data is None:
+        try:
+            with open(data_file, 'rb') as f:
+                data = pickle.load(f)
+        except FileNotFoundError:
+            time.sleep(1)
+    latest = max(list(data.keys()))
+    h = data[latest]['Humidity']
+    t = data[latest]['Temperature']
+    return h, t
+
+
+def main():
+    slack_client = SlackClient(get_slack_client_id())
+    channel = "mattpihumidor"
+    connected = False
+    while connected is False:
+        if slack_client.rtm_connect():
+            connected = True
+            send_message('Response bot is online', channel)
+            while True:
+                # Read bot user id by calling web API method auth.test
+                starterbot_id = slack_client.api_call("auth.test")["user_id"]
+                events = slack_client.rtm_read()
+                for event in events:
+                    if event["type"] == "message" and event["text"].lower() == 'report':
+                        h, t = get_latest_reading()
+                        send_message('The current humidity and temperature is {}% {}C'.format(h, t), channel)
+                    elif event['type'] == 'message' and event['text'].lower() == 'shutdown':
+                        call("sudo shutdown -h now", shell=True)
+                    elif event['type'] == 'message' and event['text'].lower() == 'reboot':
+                        call("sudo reboot", shell=True)
+                    elif event['type'] == 'message' and event['text'].lower() == 'hello':
+                        send_message('World!', channel)
+                    time.sleep(RTM_READ_DELAY)
+        else:
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        send_message("Reponse Bot had an error: {}".format(e))
