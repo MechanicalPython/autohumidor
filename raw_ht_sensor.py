1e972af373fec8f2f6bf35557429b92d19d25a1c
Matt Barson <matt.barson@gmail.com>
2020-03-10 18:47:30 +0000

    New raw_ht_sensor.py method with args (seconds). Default 60 seconds

--- raw_ht_sensor.py
+++ raw_ht_sensor.py
@@ -8,22 +8,40 @@ Measures humid and temp for a 10 second average and pushes to data.json in
 import time
 import board
 import adafruit_dht
+import statistics as stats
+import sys
 
 # Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT22(board.D18)
 
-while True:
-    try:
-        # Print the values to the serial port
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% "
-              .format(temperature_f, temperature_c, humidity))
 
-    except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
+def ht_reading(interval=60):
+    """Gives an average reading of humidity and temp for a given time interval (seconds).
+    """
+    dht = adafruit_dht.DHT22(board.D18)
+    t_end = time.time() + interval
+    hum = []
+    temp = []
+    while time.time() < t_end:
+        try:
+            temperature = dht.temperature   # Takes an indeterminant amount of time to return value.
+            humidity = dht.humidity
+            hum.append(humidity)
+            temp.append(temperature)
+        except RuntimeError:
+            pass
+    if len(hum) > 0 and len(temp) > 0:
+        return round(stats.mean(hum), 2), round(stats.mean(temp), 2)
+    else:
+        return None, None
 
-    time.sleep(2.0)
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    if len(args) > 1 and args[1].isdigit():
+        h, t = ht_reading(int(args[1]))
+    else:
+        h, t = ht_reading(60)  # 10 minutes of readings.
+    print(f'Humitidy: {h}, temp {t}')
 
